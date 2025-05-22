33a1b8d09033327a96fff311b4380816ea00c550
Timothy Partee <timothyp2099@gmail.com>
2025-05-22 13:38:19 -0700

    Data Persistence
    completed code that allow for data persistence between scenes and application exit and restart

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -15,6 +15,7 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
@@ -22,6 +23,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -36,7 +38,16 @@ public class MenuUIHandler : MonoBehaviour
 #else
         Application.Quit(); // this will quit the game
 #endif
-
+        MainManager.Instance.SaveColor();
     }
 
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
 }
